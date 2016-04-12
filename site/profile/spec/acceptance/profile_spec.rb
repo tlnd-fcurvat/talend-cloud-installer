11eb6af1725bb25291fa598a44872585abca2446
aheumaier <andreas.heumaier@nordcloud.com>
2016-04-12 18:06:21 +0200

    Fix for beaker bug BKR-537

---
+++ site/profile/spec/acceptance/profile_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper_acceptance'
+
+describe "profile", :unless => UNSUPPORTED_PLATFORMS.include?(fact('osfamily')) do
+  describe "base" do
+    it 'should provision base role' do
+      pp = <<-EOS
+        class { 'profile::base':
+        }
+      EOS
+
+      # With the version of java that ships with pe on debian wheezy, update-alternatives
+      # throws an error on the first run due to missing alternative for policytool. It still
+      # updates the alternatives for java
+      apply_manifest(pp, :catch_failures => true)
+
+      apply_manifest(pp, :catch_changes => true)
+    end
+  end
+end
