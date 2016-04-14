8bb8913aef60b098919d459bb01409cbc38398be
aheumaier <andreas.heumaier@nordcloud.com>
2016-04-14 15:59:07 +0200

    Fixes for getting repo running on aws

--- site/profile/spec/acceptance/profile_base_spec.rb
+++ site/profile/spec/acceptance/profile_base_spec.rb
@@ -11,7 +11,7 @@ describe "profile", :unless => UNSUPPORTED_PLATFORMS.include?(fact('osfamily')) 
       # With the version of java that ships with pe on debian wheezy, update-alternatives
       # throws an error on the first run due to missing alternative for policytool. It still
       # updates the alternatives for java
-      apply_manifest(pp, :catch_failures => true, :modulepath => '/tmp/puppet/site:/tmp/puppet/modules')
+      apply_manifest(pp, :catch_failures => true, :modulepath => '/tmp/puppet/site:/tmp/puppet/modules', :hiera_config => '/tmp/puppet/hiera.yaml')
 
       apply_manifest(pp, :catch_changes => true, :modulepath => '/tmp/puppet/site:/tmp/puppet/modules')
     end
