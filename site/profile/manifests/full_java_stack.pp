ce87cdb24276b11e8beda6f0989ffbd640062443
aheumaier <andreas.heumaier@nordcloud.com>
2016-04-19 16:43:10 +0200

    WIP: rspec testing for profile::full_java_stack

--- site/profile/manifests/full_java_stack.pp
+++ site/profile/manifests/full_java_stack.pp
@@ -1,30 +1,18 @@
 class profile::full_java_stack {
 
-  #include ::java
+  include ::java
   include ::nginx
-  #include ::tomcat
-
-  #tomcat::install { '/opt/tomcat/':
-  #  source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-7/v7.0.69/bin/apache-tomcat-7.0.69.tar.gz',
-  #}
-  #tomcat::instance { 'default':
-  #  catalina_home => '/opt/tomcat/',
-  #}
+  include ::tomcat
 
 
-  class { 'tomcat': }
-  class { 'java': }
+
+  #class { 'tomcat': }
+  #class { 'java': }
 
 
   tomcat::instance { 'default':
     catalina_home => '/opt/tomcat',
   }
-
-
- # tomcat::instance { 'mycat':
- #   catalina_base => '/opt/apache-tomcat/',
- #   source_url    => 'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.53/bin/apache-tomcat-7.0.53.tar.gz',
- #}
 
   #->
   # tomcat::config::server::context { 'mycat-test':
