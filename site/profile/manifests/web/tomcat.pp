e41bea4f0db336481d2319afe39c9b3fd89ad8a8
aheumaier <andreas.heumaier@nordcloud.com>
2016-04-20 15:49:10 +0200

    Better testing conditions for vagrant

--- site/profile/manifests/web/tomcat.pp
+++ site/profile/manifests/web/tomcat.pp
@@ -12,10 +12,13 @@ class profile::web::tomcat {
   profile::register_profile{ 'tomcat': }
 
   tomcat::instance { 'default':
+    source_url    => 'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.53/bin/apache-tomcat-7.0.53.tar.gz',
     catalina_home => '/opt/tomcat',
+    port          => '8080',
   }
   #
   # configuring tomcat server contexts applications from hiera
   $tomcat_server_contexts = hiera_hash('tomcat_server_contexts', {})
+  create_resources('tomcat::config::server::context', $tomcat_server_contexts)
 
 }
