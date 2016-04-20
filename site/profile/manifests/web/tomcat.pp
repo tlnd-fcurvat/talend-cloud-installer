12c37c0471b2012f17f4d1f09882b6094399676c
aheumaier <andreas.heumaier@nordcloud.com>
2016-04-20 16:13:23 +0200

    Testing tomcat module

--- site/profile/manifests/web/tomcat.pp
+++ site/profile/manifests/web/tomcat.pp
@@ -6,23 +6,48 @@
 #
 class profile::web::tomcat {
 
-  include ::java
-  include ::tomcat
 
-  profile::register_profile{ 'tomcat': }
 
-  tomcat::instance { 'default':
+  class { '::tomcat': }
+  class { '::java': }
+
+  tomcat::instance { 'mycat':
+    catalina_base => '/opt/apache-tomcat/mycat',
     source_url    => 'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.53/bin/apache-tomcat-7.0.53.tar.gz',
-    catalina_home => '/opt/tomcat',
+  }->
+  tomcat::config::server::context { 'mycat-test':
+    catalina_base         => '/opt/apache-tomcat/mycat',
+    context_ensure        => present,
+    doc_base              => 'test.war',
+    parent_service        => 'Catalina',
+    parent_engine         => 'Catalina',
+    parent_host           => 'localhost',
+    additional_attributes => {
+      'path' => '/test',
+    },
   }
 
-  tomcat::config::server{'default':
-    port    => '8080',
-    address => '127.0.0.1'
-  }
+
+
+
+  #
+  # include ::java
+  # include ::tomcat
+  #
+  # profile::register_profile{ 'tomcat': }
+  #
+  # tomcat::instance { 'default':
+  #   source_url    => 'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.53/bin/apache-tomcat-7.0.53.tar.gz',
+  #   catalina_home => '/opt/tomcat',
+  # }
+  #
+ # tomcat::config::server{'default':
+ #   port    => '8080',
+ #   address => '127.0.0.1'
+ # }
 
   # configuring tomcat server contexts applications from hiera
-  $tomcat_server_contexts = hiera_hash('tomcat_server_contexts', {})
-  create_resources('tomcat::config::server::context', $tomcat_server_contexts)
+  #$tomcat_server_contexts = hiera_hash('tomcat_server_contexts', {})
+  #create_resources('tomcat::config::server::context', $tomcat_server_contexts)
 
 }
