0742f55028d2d1edf92c4fd9d82c1dfa1d39bbd3
aheumaier <andreas.heumaier@nordcloud.com>
2016-04-20 15:35:01 +0200

    Better testing conditions for vagrant

--- site/role/spec/spec_helper_acceptance.rb
+++ site/role/spec/spec_helper_acceptance.rb
@@ -11,7 +11,7 @@ unless ENV["RS_PROVISION"] == "no" or ENV["BEAKER_provision"] == "no"
   hosts.each do |host|
     on host, 'yum -y install git gcc gcc-c++ ruby-devel libxslt-devel libxml2-devel rubygem-bundler'
     on host, "git clone https://github.com/Talend/talend-cloud-installer.git #{WORKDIR}"
-    on host, "cd #{WORKDIR} && bundle install --path=vendor/bundle --withou development"
+    on host, "cd #{WORKDIR} && bundle install --path=vendor/bundle --without development"
     on host, "cp -R #{WORKDIR}/hiera* /etc/puppet/"
   end
 end
