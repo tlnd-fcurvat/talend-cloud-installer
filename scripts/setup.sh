8d71e041094948f8f6e6788250c6237738ae89f7
Ahmed Osman <ahmed.osman@nordcloud.com>
2016-04-28 16:21:15 +0200

    Fixed variable logic

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
+if which bundle > /dev/null
+then
+    BUNDLER=`which bundle`
+fi
 
-BUNDLER=`which bundle`
-R10K=`which r10k`
-PUPPET=`which puppet`
+if which r10k > /dev/null
+then
+    R10K=`which r10k`
+fi
+
+if which puppet > /dev/null
+then
+    PUPPET=`which puppet`
+fi
 
 
-if [ -n $BUNDLER ]; then
-	${BUNDLER} install --path=vendor/bundler > /dev/null
+if [ -n $BUNDLER ]
+then
+	$BUNDLER install --path=vendor/bundler > /dev/null
 else
 	echo 'Error bundler gem not installed'
 	exit 1
 fi
 
-if [ -n $R10K ]; then
-	${R10K} puppetfile install  > /dev/null
+if [ -n $R10K ]
+then
+	$R10K puppetfile install  > /dev/null
 else
 	echo 'Error r10k gem  not installed'
 	exit 1
 fi
 
-if [ -n $PUPPET ]; then
-	${PUPPET} apply --verbose \
+if [ -n $PUPPET ]
+then
+	$PUPPET apply --verbose \
 		--noop \
 		--modulepath=site:modules \
 		--hiera_config=hiera.yaml manifests/site.pp  > /dev/null
