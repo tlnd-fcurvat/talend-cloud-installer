63123ec26db8ce909d384877fa4bf03347162f93
aheumaier <andreas.heumaier@nordcloud.com>
2016-04-29 16:48:09 +0200

    Update for role_acceptance

--- site/role/spec/acceptance/role_example_spec.rb
+++ site/role/spec/acceptance/role_example_spec.rb
@@ -33,14 +33,6 @@ describe "role::example", :unless => UNSUPPORTED_PLATFORMS.include?(fact('osfami
       it { should be_listening }
     end
 
-    describe port(8080) do
-      it { should be_listening }
-    end
-
-    describe process("tomcat-instance1") do
-      its(:user) { should eq "tomcat" }
-      its(:args) { should match /tomcat-instance1/ }
-    end
 
   end
 end
