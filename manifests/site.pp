70dfc6bd56a579fdeecba615d5979d83c06763b8
aheumaier <andreas.heumaier@nordcloud.com>
2016-05-09 14:16:32 +0200

    Changed to dymic node classifier via $::puppet_role

--- manifests/site.pp
+++ manifests/site.pp
@@ -30,9 +30,7 @@ Package {
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
-
 node default {
-  # This is where you can declare classes for all nodes.
-  # dynamic it is include "%::system_role"
-	include "role::mongodb"
+  # This is where you can declare dynamic classes for all nodes.
+  include "role::${::puppet_role}"
 }
