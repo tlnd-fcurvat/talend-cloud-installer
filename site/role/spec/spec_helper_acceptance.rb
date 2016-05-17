72742a9773b1bab5a4f703b0f47bf296bdd140d3
aheumaier <andreas.heumaier@nordcloud.com>
2016-05-17 14:31:32 +0200

    Update spe_helper for respect current git working branch

--- site/role/spec/spec_helper_acceptance.rb
+++ site/role/spec/spec_helper_acceptance.rb
@@ -6,11 +6,12 @@ run_puppet_install_helper
 
 UNSUPPORTED_PLATFORMS = [ "Darwin", "windows" ]
 WORKDIR = '/tmp/puppet'
+GIT_BRANCH = `git branch | grep '^\*'`
 
 unless ENV["RS_PROVISION"] == "no" or ENV["BEAKER_provision"] == "no"
   hosts.each do |host|
     on host, 'yum -y install git gcc gcc-c++ ruby-devel libxslt-devel libxml2-devel rubygem-bundler'
-    on host, "git clone https://github.com/Talend/talend-cloud-installer.git #{WORKDIR}"
+    on host, "git clone https://github.com/Talend/talend-cloud-installer.git #{WORKDIR} -b #{GIT_BRANCH}"
     on host, "cd #{WORKDIR} && bundle install --path=vendor/bundle --without development"
     on host, "cp -R #{WORKDIR}/hiera* /etc/puppet/"
     on host, 'mkdir -p /etc/facter/facts.d'
