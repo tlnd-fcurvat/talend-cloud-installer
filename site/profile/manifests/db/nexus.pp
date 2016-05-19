a7da3a8af62f688f25967f50add993e4d3ea0546
aheumaier <andreas.heumaier@nordcloud.com>
2016-05-19 16:21:20 +0200

    Fix for duplicate resources in nexus profile

--- site/profile/manifests/db/nexus.pp
+++ site/profile/manifests/db/nexus.pp
@@ -14,15 +14,6 @@ class profile::db::nexus (
     version    => '2.8.0',
     revision   => '05',
     nexus_root => $nexus_root, # All directories and files will be relative to this
-  }  ->
-
-  exec {
-    'disable_anonymous':
-      command => "/usr/bin/curl -X PUT -u admin:admin123 http://localhost:8081/nexus/service/local/users/anonymous -H 'Content-type: application/json' -d '{\"data\":{\"userId\":\"anonymous\",\"firstName\":\"Nexus\",\"lastName\":\"Anonymous User\",\"email\":\"changeme2@yourcompany.com\",\"status\":\"disabled\",\"roles\":[\"anonymous\",\"repository-any-read\"]}}'",
-      onlyif => '/usr/bin/curl -f http://localhost:8081/nexus/service/local/all_repositories';
-    'delete_user_deployment':
-      command => '/usr/bin/curl -s -f -X DELETE -u admin:admin123 http://localhost:8081/nexus/service/local/users/deployment',
-      onlyif => '/usr/bin/curl -s -f -X GET -u admin:admin123 http://localhost:8081/nexus/service/local/users/deployment';
   }
 
   Class['::java'] ->
