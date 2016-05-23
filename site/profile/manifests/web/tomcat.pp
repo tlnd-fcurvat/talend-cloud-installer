434af2c553d0308837e381e73c59be7ad78885a6
aheumaier <andreas.heumaier@nordcloud.com>
2016-05-23 13:43:56 +0200

    FIX: Setup oracle jdk and modificationt to puppetlabs/tomcat

--- site/profile/manifests/web/tomcat.pp
+++ site/profile/manifests/web/tomcat.pp
@@ -4,34 +4,26 @@
 # === Authors
 # Andreas Heumaier <andreas.heumaier@nordcloud.com>
 #
-class profile::web::tomcat (
+class profile::web::tomcat (){
 
 
-
-
-){
 
 
   class { '::jdk_oracle': } ->
 
-  tomcat::instance { 'instance1':
+  tomcat::instance { 'tomcat7':
     install_from_source => true,
     source_url          => 'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.53/bin/apache-tomcat-7.0.53.tar.gz',
     manage_user         => true,
     manage_group        => true,
     user                => 'tomcat',
     group               => 'tomcat',
-    catalina_base       => '/opt/apache-tomcat/instance1',
+    catalina_base       => '/opt/apache-tomcat/tomcat7',
     catalina_home       => undef,
     java_home           => '/opt/jdk-7',
   } ->
-
-  tomcat::config::server{ 'instance1':
-    catalina_base => '/opt/apache-tomcat/instance1',
-    port          => '8081',
-  } ->
-  tomcat::service { 'instance1':
-    catalina_base => '/opt/apache-tomcat/instance1',
+  tomcat::service { 'tomcat7':
+    catalina_base => '/opt/apache-tomcat/tomcat7',
     use_init      => false,
   }
 
