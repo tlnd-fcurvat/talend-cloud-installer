e75db63991a75d8be37a519cc0428bfae8ae538b
aheumaier <andreas.heumaier@nordcloud.com>
2016-05-26 16:18:33 +0200

    FIX: feature/adding_packagecloud_repo passing simple beaker

--- site/profile/spec/acceptance/profile_base_spec.rb
+++ site/profile/spec/acceptance/profile_base_spec.rb
@@ -16,9 +16,11 @@ require 'spec_helper_acceptance'
         # With the version of java that ships with pe on debian wheezy, update-alternatives
         # throws an error on the first run due to missing alternative for policytool. It still
         # updates the alternatives for java
-        apply_manifest(pp, :catch_failures => true, :modulepath => '/tmp/puppet/site:/tmp/puppet/modules', :hiera_config => '/tmp/puppet/hiera.yaml')
+        agent = only_host_with_role(hosts, 'base')
 
-        apply_manifest(pp, :catch_changes => true, :modulepath => '/tmp/puppet/site:/tmp/puppet/modules', :hiera_config => '/tmp/puppet/hiera.yaml')
+        apply_manifest_on(agent, pp, :catch_failures => true, :modulepath => '/tmp/puppet/site:/tmp/puppet/modules', :hiera_config => '/tmp/puppet/hiera.yaml')
+
+        apply_manifest_on(agent, pp, :catch_changes => true, :modulepath => '/tmp/puppet/site:/tmp/puppet/modules', :hiera_config => '/tmp/puppet/hiera.yaml')
       end
 
       context 'should have base rofile prvisioned' do
