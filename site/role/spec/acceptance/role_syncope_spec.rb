4c28ccb7acb34fb856ffc314b58fefb50864617b
aheumaier <andreas.heumaier@nordcloud.com>
2016-05-31 12:21:31 +0200

    Added syncope beaker setup

---
+++ site/role/spec/acceptance/role_syncope_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper_acceptance'
+
+
+
+
+describe "role::syncope" do
+  let(:pp) do
+    <<-EOS
+        class { 'role::syncope':
+        }
+    EOS
+  end
+
+  it_behaves_like "a idempotent resource"
+end
+
+describe 'should have syncope role configured' do
+
+  describe package('nginx') do
+    it { is_expected.to be_installed }
+  end
+
+  describe service('nginx') do
+    it { is_expected.to be_enabled }
+    it { is_expected.to be_running }
+  end
+
+  describe port(80) do
+    it { should be_listening }
+  end
+
+  it 'should have java process with correct arguments' do
+    expect(command('pgrep -a java').stdout).to match /\/opt\/tomcat\//
+  end
+
+  describe port(8080) do
+    it { should be_listening }
+  end
+
+end
