a2c641bf9a8154b16b07a2f3a5d62c72290b4e04
Alexander Grigoriev <agrigoriev@talend.com>
2016-06-01 11:43:37 +0200

    acceptance helper updated

--- site/role/spec/spec_helper_acceptance.rb
+++ site/role/spec/spec_helper_acceptance.rb
@@ -2,8 +2,6 @@ require 'beaker-rspec'
 require 'beaker/puppet_install_helper'
 require 'beaker-rspec/helpers/serverspec'
 
-run_puppet_install_helper
-
 UNSUPPORTED_PLATFORMS = [ "Darwin", "windows" ]
 WORKDIR = '/tmp/puppet'
 
@@ -13,14 +11,21 @@ else
   GIT_BRANCH = ENV['GIT_BRANCH']
 end
 
+hosts.each do |host|
+  install_puppet
+  on host, 'mkdir -p /etc/facter/facts.d'
+  create_remote_file host,
+    '/etc/facter/facts.d/role_facts.txt',
+    "puppet_role=#{host['roles'].last}\npuppet_roles=%#{host['roles'].join('%,%')}%",
+    :protocol => 'rsync'
+end
+
 unless ENV["RS_PROVISION"] == "no" or ENV["BEAKER_provision"] == "no"
   hosts.each do |host|
     on host, 'yum -y install git rubygem-bundler'
     on host, "git clone https://github.com/Talend/talend-cloud-installer.git #{WORKDIR} -b #{GIT_BRANCH}"
     on host, "cd #{WORKDIR} && bundle install --path=vendor/bundle --without development"
     on host, "cp -R #{WORKDIR}/hiera* /etc/puppet/"
-    on host, 'mkdir -p /etc/facter/facts.d'
-
   end
 end
 
@@ -35,7 +40,6 @@ RSpec.configure do |c|
   c.before :suite do
     hosts.each do |host|
       c.host = host
-      create_remote_file host, '/etc/facter/facts.d/role_facts.txt', "puppet_role=#{host['roles'].last}", :protocol => 'rsync'
       create_remote_file host, '/etc/facter/facts.d/packagecloud_facts.txt', "packagecloud_master_token=#{ENV['PACKAGECLOUD_MASTER_TOKEN']}", :protocol => 'rsync'
       on host,"cd #{WORKDIR} && bundle exec r10k puppetfile install"
     end
