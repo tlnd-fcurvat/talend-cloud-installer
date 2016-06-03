4cf576e2b99f5b8ea7de05165ee13e532c1b3bff
Alexander Grigoriev <agrigoriev@talend.com>
2016-06-03 11:12:53 +0200

    extending elasticsearch role with some minor improvements

--- site/role/manifests/elasticsearch.pp
+++ site/role/manifests/elasticsearch.pp
@@ -3,8 +3,17 @@
 class role::elasticsearch {
 
   include ::profile::base
-  include java
+  include ::java
   include ::profile::elasticsearch
 
-  Class['java'] -> Class['::profile::elasticsearch']
+  ensure_packages(['wget'])
+
+  exec { 'waiting for Elasticsearch to start':
+    command => '/usr/bin/wget --spider --tries 15 --retry-connrefused "http://localhost:9200/?pretty"'
+  }
+
+  Class['::java'] ->
+  Class['::profile::elasticsearch'] ->
+  Package['wget'] ->
+  Exec['waiting for Elasticsearch to start']
 }
