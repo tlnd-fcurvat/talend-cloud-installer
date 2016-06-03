5e305184b6bdbd187442cd34423d8317eee420b5
Alexander Grigoriev <agrigoriev@talend.com>
2016-06-03 11:34:25 +0200

    removing obsolete code

--- site/role/spec/spec_helper_acceptance.rb
+++ site/role/spec/spec_helper_acceptance.rb
@@ -8,27 +8,6 @@ WORKDIR = '/tmp/puppet'
 base_spec_dir = Pathname.new(File.join(File.dirname(__FILE__), 'acceptance'))
 Dir[base_spec_dir.join('shared/**/*.rb')].sort.each{ |f| require f }
 
-# Install puppet on hosts, as well as provision them with role facts
-hosts.each do |host|
-  install_puppet
-  on host, 'mkdir -p /etc/facter/facts.d'
-  create_remote_file host,
-    '/etc/facter/facts.d/role_facts.txt',
-    "puppet_role=#{host['roles'].first}\npuppet_roles=#{host['roles'].join(',')}",
-    :protocol => 'rsync'
-end
-
-# Deploy codebase on hosts
-hosts.each do |host|
-  rsync_to host,
-    File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..' )), # control repo root
-    WORKDIR
-  on host, 'yum -y install epel-release'
-  on host, 'yum -y install git rubygem-bundler ruby-augeas'
-  on host, "cd #{WORKDIR} && bundle install --path=vendor/bundle --without development"
-  on host, "cp -R #{WORKDIR}/hiera* /etc/puppet/"
-end
-
 RSpec.configure do |c|
   # Project root
   proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..' ))
