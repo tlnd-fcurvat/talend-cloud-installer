53ef404e2ca3a0376d3bab0c2f7e84c5a831a90a
aheumaier <andreas.heumaier@nordcloud.com>
2016-06-20 09:51:41 +0200

    Set the spec matcher for parsing awslos.log

--- site/profile/spec/acceptance/profile_base_spec.rb
+++ site/profile/spec/acceptance/profile_base_spec.rb
@@ -32,5 +32,13 @@ describe 'should have base profile configured' do
   # describe package('aws-sdk') do
   #   it { is_expected.to be_installed.by('pip') }
   # end
+
+  describe file('/var/log/awslogs.log') do
+    its(:content) { should match /Log group:\ \/talend\/tic\/base\/var\/log\/audit,\ log stream/ }
+    its(:content) { should match /Log group:\ \/talend\/tic\/base\/var\/log\/messages,\ log stream/ }
+    its(:content) { should match /Log group:\ \/talend\/tic\/base\/var\/log\/secure,\ log stream/ }
+    its(:content) { should_not match /ERROR:/ }
+  end
+
 end
 
