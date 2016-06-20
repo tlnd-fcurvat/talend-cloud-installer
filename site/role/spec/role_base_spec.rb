867bc3b1ab913509dbe791741de8459ecf68f132
aheumaier <andreas.heumaier@nordcloud.com>
2016-06-20 09:55:19 +0200

    Update for site/role: Set the spec matcher for parsing awslos.log

--- site/role/spec/role_base_spec.rb
+++ site/role/spec/role_base_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper_acceptance'
 
 describe "role::base" , :if => fact('puppet_roles').split(',').include('base') do
   it_behaves_like 'puppet::appliable', 'include "role::base"'
+
+
+  describe file('/var/log/awslogs.log') do
+    its(:content) { should match /Log group:\ \/talend\/tic\/base\/var\/log\/audit,\ log stream/ }
+    its(:content) { should match /Log group:\ \/talend\/tic\/base\/var\/log\/messages,\ log stream/ }
+    its(:content) { should match /Log group:\ \/talend\/tic\/base\/var\/log\/secure,\ log stream/ }
+    its(:content) { should_not match /ERROR:/ }
+  end
+
 end
