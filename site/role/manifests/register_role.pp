0f2534479f97e0d5777feb5193bd7d240f989691
Alexander Grigoriev <agrigoriev@talend.com>
2016-07-05 11:52:44 +0200

    code style for the register_role resource

--- site/role/manifests/register_role.pp
+++ site/role/manifests/register_role.pp
@@ -1,13 +1,23 @@
+#
 # used by other modules to register themselves in the system_role
-define role::register_role($id='', $order=10,$content=undef,) {
+#
+define role::register_role(
+
+  $id      = '',
+  $order   = 10,
+  $content = undef,
+
+) {
+
   if $content == '' {
     $body = $name
   } else {
     $body = $content
   }
 
-  concat::fragment{"10_fragment_role_${name}":
+  concat::fragment { "10_fragment_role_${name}":
     target  => '/etc/sysconfig/puppetRole',
     content => "${body}\n"
   }
+
 }
