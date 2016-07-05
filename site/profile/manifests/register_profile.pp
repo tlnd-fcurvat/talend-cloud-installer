ec516569a6252480795c0f55a5f17f10ef441d49
Alexander Grigoriev <agrigoriev@talend.com>
2016-07-05 09:54:23 +0200

    register_profile resource definition updated

--- site/profile/manifests/register_profile.pp
+++ site/profile/manifests/register_profile.pp
@@ -1,13 +1,21 @@
-# used by other modules to register themselves in the system_role
-define profile::register_profile($id='', $order=10,$content=undef) {
+#
+# used by other modules to register themselves in the system_profile
+#
+define profile::register_profile (
+  $id      = '',
+  $order   = 10,
+  $content = undef,
+) {
+
   if $content == '' {
     $body = $name
   } else {
     $body = $content
   }
 
-  concat::fragment{"10_fragment_profile_${name}":
+  concat::fragment { "10_fragment_profile_${name}":
     target  => '/etc/sysconfig/puppetProfile',
     content => "${body}\n"
   }
+
 }
