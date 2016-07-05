ab9c10e0f308c76c9872afd0c0cc0b4444f2cc5f
Alexander Grigoriev <agrigoriev@talend.com>
2016-07-05 10:55:00 +0200

    nexus profile refactoring

---
+++ site/profile/manifests/nexus.pp
@@ -0,0 +1,24 @@
+#
+# Sets up the nexus instance
+#
+class profile::nexus (
+
+  $nexus_root = '/srv',
+
+) {
+
+  require ::profile::java
+  require ::profile::common::packages
+
+  include ::profile::common::concat
+
+  profile::register_profile { 'nexus': }
+
+  class { '::nexus':
+    version    => '2.8.0',
+    revision   => '05',
+    nexus_root => $nexus_root, # All directories and files will be relative to this
+  }
+  contain ::nexus
+
+}
