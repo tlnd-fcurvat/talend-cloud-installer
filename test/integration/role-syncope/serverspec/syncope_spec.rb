f5e5a8ef0baa4a615b6267a4a8c055cf28f67813
Alexander Grigoriev <agrigoriev@talend.com>
2016-07-05 14:51:51 +0200

    fixing typo

--- test/integration/role-syncope/serverspec/syncope_spec.rb
+++ test/integration/role-syncope/serverspec/syncope_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'role::syncope' do
   it_behaves_like 'profile::base'
-  it_behaves_like 'profile::syncope'
+  it_behaves_like 'profile::web::syncope'
   it_behaves_like 'role::defined', 'syncope'
 end
