2b0acf422f20c5935f0130135971e19d70533900
Stefan Baryakov <sbarakov@talend.com>
2016-07-10 12:15:14 +0000

    [DEVOPS-703] add missing directories

--- site/profile/manifests/tic_services.pp
+++ site/profile/manifests/tic_services.pp
@@ -6,4 +6,9 @@ class profile::tic_services {
   require ::profile::base
   require ::profile::java
 
+  # Workaround for DEVOPS-703
+  file {
+    ['/opt/talend', '/opt/talend/ipaas']:
+        ensure => directory
+  }
 }
