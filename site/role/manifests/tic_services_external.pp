764a6cb3ed6e638fc4cca9df650e370d537f50ff
Stefan Baryakov <sbarakov@talend.com>
2016-07-10 15:29:57 +0000

    WIP: integration of single of role

--- site/role/manifests/tic_services_external.pp
+++ site/role/manifests/tic_services_external.pp
@@ -5,19 +5,98 @@ class role::tic_services_external {
 
   require profile::tic_services
 
-	class { 'tic::globals':
-		role                               => 'services',
-		hiera_dts_s3_bucket_test_data      => 'us-east-1-rd-tipaas-dts-test-talend-com',
-		hiera_dts_s3_bucket_rejected_data  => 'us-east-1-rd-tipaas-dts-rejected-talend-com',
-		hiera_dts_s3_bucket_logs_data      => 'us-east-1-rd-tipaas-dts-logs-talend-com',
-		hiera_dts_s3_bucket_downloads_data => 'us-east-1-rd-tipaas-dts-downloads-talend-com',
-		karaf_service_ensure               => running,
-		java_xmx                           => 1024,
-		web_enable_test_context            => false,
-		web_use_ssl                        => true,
-		cms_node                           => 'cmsnode',
-		java_home                          => '/usr/java/jre1.8.0_60',
-	} ->
-	class { 'tic': }
+  $common_features = [
+    '(',
+    'aries-blueprint',
+    'bundle',
+    'config',
+    'deployer',
+    'diagnostic',
+    'feature',
+    'instance',
+    'jaas',
+    'kar',
+    'log',
+    'management',
+    'package',
+    'service',
+    'shell',
+    'shell-compat',
+    'ssh',
+    'system',
+    ')',
+    'wrap',
+    'camel',
+    'tipaas-crypto-service-client',
+    'tipaas-artifact-manager-client-runtime',
+    'tipaas-bookkeeper-client',
+    'tipaas-account-manager-client',
+    'tipaas-scheduler-client',
+    'tipaas-dispatcher-client',
+    'tipaas-plan-executor-client',
+    'tipaas-flow-manager-client',
+    'tipaas-healthcheck-service-core',
+    'tipaas-container-management-service-client',
+    'tipaas-configuration-service-client',
+    'tipaas-webhooks-client',
+    'tipaas-notification-client',
+    'tipaas-notification-subscription-client',
+    'tipaas-data-transfer-service-client',
+  ]
+
+  $additional_features = [
+    'tipaas-zookeeper-server',
+    'tipaas-crypto-service-core',
+    'tipaas-bookkeeper-service',
+    'tipaas-data-transfer-service-core',
+    'tipaas-artifact-manager-service',
+    'tipaas-account-manager-service',
+    'tipaas-dispatcher-core',
+    'tipaas-scheduler',
+    'tipaas-plan-executor-service',
+    'tipaas-flow-manager-service',
+    'tipaas-container-management-service',
+    'tipaas-pairing-service',
+    'tipaas-configuration-service-core',
+    'tipaas-schema-discovery-service-core',
+    'tesb-el-elasticsearch',
+    'tesb-el-server',
+    'tesb-el-collector-jms',
+    'tipaas-webhooks-service',
+    'tipaas-trial-registration-service',
+    'tipaas-plan-executor-service',
+    'tipaas-custom-resources-service',
+    'tipaas-notification-subscription-service',
+    'tipaas-notification-manager',
+    'tipaas-notification-server',
+    'tipaas-notification-sendgrid-webhook-service'
+  ]
+
+
+  class { 'tic::globals':
+    role                               => 'services',
+    hiera_dts_s3_bucket_test_data      => 'us-east-1-rd-tipaas-dts-test-talend-com',
+    hiera_dts_s3_bucket_rejected_data  => 'us-east-1-rd-tipaas-dts-rejected-talend-com',
+    hiera_dts_s3_bucket_logs_data      => 'us-east-1-rd-tipaas-dts-logs-talend-com',
+    hiera_dts_s3_bucket_downloads_data => 'us-east-1-rd-tipaas-dts-downloads-talend-com',
+    karaf_service_ensure               => running,
+    java_xmx                           => 1024,
+    web_enable_test_context            => false,
+    web_use_ssl                        => true,
+    cms_node                           => 'cmsnode',
+    java_home                          => '/usr/java/jre1.8.0_60',
+
+    # WIP Integration settings
+    hiera_karaf_base_features_install        => $common_features,
+    hiera_karaf_additional_features_install  => $additional_features,
+    default_activemq_nodes                   => $::active_mq,
+    hiera_zookeeper_nodes                    => $::zookeeper,
+    hiera_postgres_db_host                   => $::db,
+    default_nexus_nodes                      => $::nexus,
+    hiera_elasticsearch_host                 => $::elastic_search
+  } ->
+
+  class { 'tic': }
 
 }
+
