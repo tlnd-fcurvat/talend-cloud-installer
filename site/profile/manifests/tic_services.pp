9dc99018e6bbaf5d5d0f472a85ed9ef2afbbbe1f
Alexander Grigoriev <agrigoriev@talend.com>
2016-07-12 08:28:18 +0200

    adding base profiles for the tic_services profile

--- site/profile/manifests/tic_services.pp
+++ site/profile/manifests/tic_services.pp
@@ -6,6 +6,11 @@ class profile::tic_services {
   require ::profile::base
   require ::profile::java
 
+  include ::profile::common::concat
+  include ::profile::common::cloudwatchlogs
+
+  profile::register_profile { 'tic_services': }
+
   # Workaround for DEVOPS-703
   file {
     ['/opt/talend', '/opt/talend/ipaas']:
