35e00d63fb82e42cb99545cb317cbd48193d3797
Alexander Grigoriev <agrigoriev@talend.com>
2016-08-03 08:31:03 +0200

    updating the frontend role to the latest version

--- site/role/manifests/frontend.pp
+++ site/role/manifests/frontend.pp
@@ -11,13 +11,6 @@ class role::frontend {
 
   role::register_role { 'frontend': }
 
-  class { 'tic::globals':
-    role                    => 'frontend',
-    java_xmx                => 1024,
-    web_enable_test_context => false,
-    web_use_ssl             => true,
-    cms_node                => 'cmsnode'
-  } ->
-  class { 'tic': }
+  class { '::tic::frontend': }
 
 }
