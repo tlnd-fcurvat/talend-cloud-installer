07739b377c7f8705c9b2f506b3e96d4949bc50c9
Alexander Grigoriev <agrigoriev@talend.com>
2016-08-24 14:11:44 +0200

    updating zookeeper tests

--- spec/acceptance/shared/zookeeper.rb
+++ spec/acceptance/shared/zookeeper.rb
@@ -10,8 +10,8 @@ shared_examples 'profile::zookeeper' do
     it { should be_listening }
   end
 
-  describe command('/usr/bin/wget -O - http://localhost:8080/exhibitor/exhibitor/v1/cluster/state') do
-    its(:stdout) { should include '"description":"serving"' }
+  describe command('/usr/bin/wget -O - http://127.0.0.1:8080/exhibitor/v1/cluster/state') do
+    its(:stdout) { should match /"description":"(serving|latent)"/ }
   end
 
 end
