674ea58a5fbdd79e3a4af2d3d13085860f2e3326
Alexander Grigoriev <agrigoriev@talend.com>
2016-08-25 11:17:46 +0200

    proper handling of the zookeeper_nodes fact

--- site/profile/manifests/zookeeper.pp
+++ site/profile/manifests/zookeeper.pp
@@ -1,7 +1,11 @@
 #
 # Zookeeper service profile
 #
-class profile::zookeeper {
+class profile::zookeeper (
+
+  $zookeeper_nodes = '', # A string f.e. '[ "10.0.2.12", "10.0.2.23" ]'
+
+) {
 
   require ::profile::common::packagecloud_repos
   require ::profile::java
@@ -11,8 +15,13 @@ class profile::zookeeper {
 
   profile::register_profile { 'zookeeper': }
 
+  # [ "10.0.2.12", "10.0.2.23" ]
+  $_zookeeper_nodes = split(regsubst($zookeeper_nodes, '[\s\[\]\"]', '', 'G'), ',')
+
+  fail($_zookeeper_nodes)
+
   class { '::zookeeper':
+    zookeeper_nodes => $_zookeeper_nodes
   }
-  contain ::zookeeper
 
 }
