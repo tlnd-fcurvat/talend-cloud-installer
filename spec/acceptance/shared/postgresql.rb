a8ce2431dfc4458b1e7ba7ec64cb361e99752d0a
Alexander Grigoriev <agrigoriev@talend.com>
2016-08-29 15:38:19 +0200

    updating the specs

--- spec/acceptance/shared/postgresql.rb
+++ spec/acceptance/shared/postgresql.rb
@@ -9,11 +9,6 @@ shared_examples 'profile::postgresql' do
     it { is_expected.to be_installed }
   end
 
-  describe service('postgresql') do
-    it { is_expected.to be_enabled }
-    it { is_expected.to be_running }
-  end
-
   describe port(5432) do
     it { should be_listening }
   end
