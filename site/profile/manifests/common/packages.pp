3b75589ca96e1d579d42727f88d0eea3c27536b9
Alexander Grigoriev <agrigoriev@talend.com>
2016-08-30 11:32:07 +0200

    added dependency between common::packages and common::repos

--- site/profile/manifests/common/packages.pp
+++ site/profile/manifests/common/packages.pp
@@ -2,8 +2,12 @@
 # Installs hiera-defined common_packages
 #
 class profile::common::packages {
+
+  require ::profile::common::packagecloud_repos
+
   create_resources(
     Package,
     hiera_hash('common_packages', {})
   )
+
 }
