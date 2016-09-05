7d26c4dc5997f4f992a4a412bf097d172922c463
Alexander Grigoriev <agrigoriev@talend.com>
2016-09-05 13:33:25 +0200

    setting up admin user for mongodb

--- site/profile/manifests/mongodb.pp
+++ site/profile/manifests/mongodb.pp
@@ -9,10 +9,27 @@ class profile::mongodb {
 
   profile::register_profile { 'mongodb': }
 
+  $create_admin_user = "/usr/bin/mongo ipaas --eval \"db.createUser({ \
+    user: 'admin', \
+    pwd: '${::master_password}', \
+    roles: [ \
+      {role: 'userAdminAnyDatabase', db: 'admin'}, \
+      {role: 'dbAdminAnyDatabase', db: 'admin'}, \
+      {role: 'readWriteAnyDatabase', db: 'admin'}, \
+      {role: 'dbOwner', db: 'ipaas'} \
+    ] \
+});\" && /bin/touch /var/lock/mongo_admin_user_lock"
+
   class { '::mongodb::server':
     verbose => true,
     auth    => true,
-    bind_ip => $::ipaddress
+    bind_ip => [$::ipaddress, '127.0.0.1'],
+  } ->
+  class { '::mongodb::client':
+  } ->
+  exec { 'setup MongoDB admin user':
+    command => $create_admin_user,
+    creates => '/var/lock/mongo_admin_user_lock',
   }
 
   contain ::mongodb::server
