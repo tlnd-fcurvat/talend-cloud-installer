cc085f1c33e881d572363caf1b50e01f7cd8c302
Alexander Grigoriev <agrigoriev@talend.com>
2016-09-05 09:51:45 +0200

    updating syncope spec, for the role doesn't provision the db

--- spec/acceptance/syncope_spec.rb
+++ spec/acceptance/syncope_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe 'role::syncope' do
   it_behaves_like 'profile::base'
-  it_behaves_like 'profile::postgresql', 'syncope', %w(syncopeconf qrtz_triggers)
   it_behaves_like 'profile::web::syncope'
   it_behaves_like 'role::defined', 'syncope'
 end
