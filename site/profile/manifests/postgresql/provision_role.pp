2b069942e0f1529d3630a117167831a686b2fd70
Alexander Grigoriev <agrigoriev@talend.com>
2016-09-06 15:34:43 +0200

    fixing order of files when provisioning postgres

--- site/profile/manifests/postgresql/provision_role.pp
+++ site/profile/manifests/postgresql/provision_role.pp
@@ -5,9 +5,20 @@ define profile::postgresql::provision_role {
 
   $role = $profile::postgresql::roles[$name]
 
-  $files = pick($role['files'], [])
-  profile::postgresql::provision_role_with_file { $files:
-    role_name => $name,
+  $role_safe = regsubst($name, '[^0-9a-zA-Z]', '_', 'G')
+  $creates   = "/var/tmp/${role_safe}.provision.done"
+
+  $host   = $profile::postgresql::hostname
+  $user   = $name
+  $pass   = pick($role['password'], $profile::postgresql::password)
+  $dbname = $name
+
+  $files = join(pick($role['files'], []), ' ')
+
+  exec { "provision ${name} with files : exec":
+    environment => "PGPASSWORD=${pass}",
+    command     => "/usr/bin/cat ${files} | /usr/bin/psql -U ${user} -h ${host} -d ${dbname} -f - && touch ${creates}",
+    creates     => $creates
   }
 
 }
