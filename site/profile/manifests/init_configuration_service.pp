9cf71dc4558292b05821c58ede28dafdb47c0512
Alexander Grigoriev <agrigoriev@talend.com>
2016-09-12 13:44:03 +0200

    added init_configuration_service profile

---
+++ site/profile/manifests/init_configuration_service.pp
@@ -0,0 +1,34 @@
+class profile::init_configuration_service (
+
+  $ami_id           = 'test',
+  $security_groups  = 'test',
+  $instance_type    = 'test',
+  $subnet_id        = 'test',
+  $instance_profile = 'test',
+
+) {
+
+  include ::profile::common::concat
+
+  profile::register_profile { 'init_configuration_service': }
+
+  $service_auth = "-u ${::services_username}:${::services_password}"
+
+  $init_configuration_service_cmd = "/usr/bin/curl --fail \
+  -X PUT \
+  -d @/var/tmp/init_configuration_service.json \
+  -H 'Content-Type: application/json' \
+  ${service_auth} \
+  http://localhost:8181/services/configuration-service/default"
+
+  file { '/var/tmp/init_configuration_service.json':
+    content => template('installer/var/tmp/init_configuration_service.json.erb'),
+  } ->
+  exec { 'init configuration-service':
+    command   => $init_configuration_service_cmd,
+    tries     => 30,
+    try_sleep => 10,
+    unless    => "/usr/bin/curl http://localhost:8181/services/configuration-service/default?p=nodeman.ami.version_1_3 ${service_auth} | grep ${ami_id}"
+  }
+
+}
