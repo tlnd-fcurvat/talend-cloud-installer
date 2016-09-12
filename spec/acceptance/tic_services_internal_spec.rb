38c5e0d56c4a82301f746d82831a4b799485e814
Alexander Grigoriev <agrigoriev@talend.com>
2016-09-12 13:44:42 +0200

    spec updated

--- spec/acceptance/tic_services_internal_spec.rb
+++ spec/acceptance/tic_services_internal_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'role::tic_services_internal' do
   it_behaves_like 'profile::base'
+  it_behaves_like 'profile::defined', 'init_configuration_service'
   it_behaves_like 'role::defined', 'tic_services_internal'
 
 	describe package('talend-ipaas-rt-infra') do
@@ -26,5 +27,14 @@ describe 'role::tic_services_internal' do
     it { should match /wrapper.java.additional.13\s*=\s*-Dcom.sun.management.jmxremote.ssl=false/ }
     it { should match /wrapper.java.maxmemory\s*=\s*1024/ }
     it { should match /wrapper.disable_restarts\s*=\s*true/ }
+  end
+
+  describe 'Configuration service default settings' do
+    subject  { command('PGPASSWORD=mypassword /usr/bin/psql -P pager -h localhost -U config -d config -c "select * from default_config_params;"') }
+    its(:stdout) { should include 'test-ami_id' }
+    its(:stdout) { should include 'test-security_groups' }
+    its(:stdout) { should include 'test-instance_type' }
+    its(:stdout) { should include 'test-subnet_id' }
+    its(:stdout) { should include 'test-instance_profile' }
   end
 end
