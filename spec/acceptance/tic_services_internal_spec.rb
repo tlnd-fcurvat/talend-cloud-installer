c323f52ecc3e753f7017d32f5f9e04301c5e828a
Alexander Grigoriev <agrigoriev@talend.com>
2016-09-13 10:17:53 +0200

    removing obsolete specs

--- spec/acceptance/tic_services_internal_spec.rb
+++ spec/acceptance/tic_services_internal_spec.rb
@@ -27,13 +27,4 @@ describe 'role::tic_services_internal' do
     it { should match /wrapper.java.maxmemory\s*=\s*1024/ }
     it { should match /wrapper.disable_restarts\s*=\s*true/ }
   end
-
-  describe 'Configuration service default settings' do
-    subject  { command('PGPASSWORD=mypassword /usr/bin/psql -P pager -h localhost -U config -d config -c "select * from default_config_params;"') }
-    its(:stdout) { should include 'test-ami_id' }
-    its(:stdout) { should include 'test-security_groups' }
-    its(:stdout) { should include 'test-instance_type' }
-    its(:stdout) { should include 'test-subnet_id' }
-    its(:stdout) { should include 'test-instance_profile' }
-  end
 end
