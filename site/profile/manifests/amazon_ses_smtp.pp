d72c52d3bc9ad848e06660884c69d0fa8937f892
Alexander Grigoriev <agrigoriev@talend.com>
2016-09-15 11:45:42 +0200

    explicitly specifying region for AmazonSES

--- site/profile/manifests/amazon_ses_smtp.pp
+++ site/profile/manifests/amazon_ses_smtp.pp
@@ -3,6 +3,7 @@ class profile::amazon_ses_smtp (
   $domain                 = undef,
   $smtp_accces_key_id     = undef,
   $smtp_secret_access_key = undef,
+  $region                 = undef,
 
 ) {
 
@@ -11,6 +12,7 @@ class profile::amazon_ses_smtp (
     smtp_username => $smtp_accces_key_id,
     smtp_password => amazon_ses_smtp_password($smtp_secret_access_key),
     smtp_port     => 25,
+    ses_region    => $region,
   }
 
 }
