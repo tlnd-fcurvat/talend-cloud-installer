de7d094963f7f8f299395132bfca61da753d7b9a
Alexander Grigoriev <agrigoriev@talend.com>
2016-10-11 09:27:01 +0200

    updating the specs

--- spec/acceptance/shared/common/packages.rb
+++ spec/acceptance/shared/common/packages.rb
@@ -1,18 +1,9 @@
 shared_examples 'profile::common::packages' do
 
-  describe package('epel-release') do
-    it { should be_installed }
-  end
-
   %w(hiera-eyaml hiera-eyaml-kms aws-sdk package_cloud).each do |p|
     describe package(p) do
       it { should be_installed.by('gem') }
     end
-  end
-
-  describe yumrepo('epel') do
-    it { should exist }
-    it { should be_enabled }
   end
 
 end
