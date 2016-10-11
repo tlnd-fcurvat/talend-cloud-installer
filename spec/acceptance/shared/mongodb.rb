fe5724c8194cdb5ad98eb02ac83a15b354bbc7bd
Alexander Grigoriev <agrigoriev@talend.com>
2016-10-11 14:21:43 +0200

    specs updated

--- spec/acceptance/shared/mongodb.rb
+++ spec/acceptance/shared/mongodb.rb
@@ -3,10 +3,6 @@ shared_examples 'profile::mongodb' do
   it_behaves_like 'profile::defined', 'mongodb'
   it_behaves_like 'profile::common::packages'
 
-  describe package('mongodb-server') do
-    it { is_expected.to be_installed }
-  end
-
   describe service('mongod') do
     it { is_expected.to be_enabled }
     it { is_expected.to be_running }
