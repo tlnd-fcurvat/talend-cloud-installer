2b784f729421f997ee50eb156c966c80115eb956
Alexander Grigoriev <agrigoriev@talend.com>
2016-10-12 14:10:38 +0200

    fixing specs

--- spec/acceptance/shared/common/packages.rb
+++ spec/acceptance/shared/common/packages.rb
@@ -1,6 +1,10 @@
 shared_examples 'profile::common::packages' do
 
-  %w(hiera-eyaml hiera-eyaml-kms aws-sdk package_cloud cloud-init).each do |p|
+  describe package('cloud-init') do
+    it { should be_installed }
+  end
+
+  %w(hiera-eyaml hiera-eyaml-kms aws-sdk package_cloud).each do |p|
     describe package(p) do
       it { should be_installed.by('gem') }
     end
