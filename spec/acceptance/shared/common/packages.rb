4b24716b8991d89ba8a254fcadf6a5827e397e70
Alexander Grigoriev <agrigoriev@talend.com>
2016-10-12 14:08:10 +0200

    installing cloud-init

--- spec/acceptance/shared/common/packages.rb
+++ spec/acceptance/shared/common/packages.rb
@@ -1,6 +1,6 @@
 shared_examples 'profile::common::packages' do
 
-  %w(hiera-eyaml hiera-eyaml-kms aws-sdk package_cloud).each do |p|
+  %w(hiera-eyaml hiera-eyaml-kms aws-sdk package_cloud cloud-init).each do |p|
     describe package(p) do
       it { should be_installed.by('gem') }
     end
