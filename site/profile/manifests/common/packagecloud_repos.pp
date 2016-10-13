81eac0471160f660a3fd58ef782704b9ddad1807
Alexander Grigoriev <agrigoriev@talend.com>
2016-10-13 13:40:48 +0200

    do not set up packagecloud_repos in case if there is no token

--- site/profile/manifests/common/packagecloud_repos.pp
+++ site/profile/manifests/common/packagecloud_repos.pp
@@ -2,8 +2,12 @@
 # Defines hiera-defined packagecloud repositories
 #
 class profile::common::packagecloud_repos {
-  create_resources(
-    'packagecloud::repo',
-    hiera_hash('packagecloud_repos', {})
-  )
+
+  if size($::packagecloud_master_token) > 0 {
+    create_resources(
+      'packagecloud::repo',
+      hiera_hash('packagecloud_repos', {})
+    )
+  }
+
 }
