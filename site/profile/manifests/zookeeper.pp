66b4ab73c794ffeb8036024e61ab49001b4f3b27
Alexander Grigoriev <agrigoriev@talend.com>
2016-10-26 15:29:36 +0200

    parametrizing zookeeper with the list of ec2 hostnames

--- site/profile/manifests/zookeeper.pp
+++ site/profile/manifests/zookeeper.pp
@@ -15,11 +15,15 @@ class profile::zookeeper (
 
   profile::register_profile { 'zookeeper': }
 
-  # [ "10.0.2.12", "10.0.2.23" ]
+  # "['10.72.4.7', '10.72.5.111', '10.72.6.51']" => ["10.72.4.7", "10.72.5.111", "10.72.6.51"]
   $_zookeeper_nodes = split(regsubst($zookeeper_nodes, '[\s\[\]\"]', '', 'G'), ',')
+  $_zookeeper_ips   = concat([$::ipaddress], $_zookeeper_nodes)
+
+  # ["10.72.4.7", "10.72.5.111", "10.72.6.51"] => ["ip-10-72-4-7", "ip-10-72-5-111", "ip-10-72-6-51"]
+  $_zookeeper_ec2_hostnames = ip2ec2hostname($_zookeeper_ips)
 
   class { '::zookeeper':
-    zookeeper_nodes => $_zookeeper_nodes
+    zookeeper_nodes => $_zookeeper_ec2_hostnames
   }
 
 }
