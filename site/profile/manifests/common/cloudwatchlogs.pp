5385fdf85bd7e7f55120c427aabe27896204e34b
Alexander Grigoriev <agrigoriev@talend.com>
2016-10-26 11:01:12 +0200

    introducing option to do not configure cloudwatchlogs
      This is done in order to prevent the awslogs service from
      being enabled/installed during ami build for it ends up with issue
      while packer tries to unmount the filesystem

--- site/profile/manifests/common/cloudwatchlogs.pp
+++ site/profile/manifests/common/cloudwatchlogs.pp
@@ -1,9 +1,15 @@
 #
 # Configures cloudwaatch with hiera-defined log files
 #
-class profile::common::cloudwatchlogs {
-  create_resources(
-    '::cloudwatchlogs::log',
-    hiera_hash('cloudwatchlog_files', {})
-  )
+class profile::common::cloudwatchlogs (
+
+  $include = true,
+
+) {
+  if $include {
+    create_resources(
+      '::cloudwatchlogs::log',
+      hiera_hash('cloudwatchlog_files', {})
+    )
+  }
 }
