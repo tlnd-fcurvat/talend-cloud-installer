24591e0b4c6d881e59d951a04432cc553ae36544
Alexander Grigoriev <agrigoriev@talend.com>
2016-10-31 08:42:10 +0100

    added parameter to do not install amazon-ssm-agent

--- site/profile/manifests/common/ssm.pp
+++ site/profile/manifests/common/ssm.pp
@@ -3,26 +3,29 @@
 #
 class profile::common::ssm (
 
+  $include        = true,
   $region         = undef,
   $service_enable = true,
   $service_ensure = running,
 
 ) {
 
-  $url  = "amazon-ssm-${region}.s3.amazonaws.com/latest/linux_amd64/amazon-ssm-agent.rpm"
-  $path = '/opt/amazon-ssm-agent.rpm'
+  if $include {
+    $url  = "amazon-ssm-${region}.s3.amazonaws.com/latest/linux_amd64/amazon-ssm-agent.rpm"
+    $path = '/opt/amazon-ssm-agent.rpm'
 
-  exec { 'download-ssm-agent':
-    command => "/usr/bin/wget -T60 -N https://${url} -O ${path}",
-    path    => '/bin:/usr/bin:/usr/local/bin:/usr/sbin',
-    creates => $path,
-  } ->
-  package { 'amazon-ssm-agent':
-    source => $path,
-  } ->
-  service { 'amazon-ssm-agent':
-    ensure => $service_ensure,
-    enable => $service_enable,
+    exec { 'download-ssm-agent':
+      command => "/usr/bin/wget -T60 -N https://${url} -O ${path}",
+      path    => '/bin:/usr/bin:/usr/local/bin:/usr/sbin',
+      creates => $path,
+    } ->
+    package { 'amazon-ssm-agent':
+      source => $path,
+    } ->
+    service { 'amazon-ssm-agent':
+      ensure => $service_ensure,
+      enable => $service_enable,
+    }
   }
 
 }
