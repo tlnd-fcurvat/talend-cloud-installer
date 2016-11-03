eba7a9e5564198109e8b95cb965c9fd4ea5e48b7
Stefan Baryakov <sbaryakov@talend.com>
2016-11-03 19:42:47 +0100

    replica set creation

--- site/profile/manifests/mongodb.pp
+++ site/profile/manifests/mongodb.pp
@@ -1,13 +1,45 @@
 #
 # Sets up the mongodb instance
 #
-class profile::mongodb {
+class profile::mongodb(
+  $shared_key = undef
+) {
 
   require ::profile::common::packages
 
   include ::profile::common::concat
 
   profile::register_profile { 'mongodb': }
+
+  $_mongo_nodes = parsejson($::mongodb_nodes)
+
+  # explicitly only support replica sets of size 3
+  if size($_mongo_nodes)  == '3' {
+    $_mongo_members = suffix($_mongo_nodes, ':27017')
+
+    $replset_name = 'tipaas'
+
+    $replset_config = {
+      'tipaas' => {
+        ensure  => 'present',
+        members => $_mongo_members
+      }
+    }
+
+    if $shared_key {
+      $mongo_auth = true
+      $keyfile = '/var/lib/mongo/shared_key'
+    } else {
+      $mongo_auth = false
+      $keyfile = undef
+    }
+
+  } else {
+    $mongo_replset_name = undef
+    $replset_name = undef
+    $mongo_auth = true
+  }
+
 
   $create_admin_user = "/usr/bin/mongo ipaas --eval \"db.createUser({ \
     user: 'admin', \
@@ -24,18 +56,29 @@ class profile::mongodb {
     manage_package_repo => true,
   }->
   class { '::mongodb::server':
-    verbose => true,
-    auth    => true,
-    bind_ip => [$::ipaddress, '127.0.0.1'],
+    verbose        => true,
+    auth           => $mongo_auth,
+    bind_ip        => [$::ipaddress, '127.0.0.1'],
+    replset        => $replset_name,
+    replset_config => $replset_config,
+    key            => $shared_key,
+    keyfile        => $keyfile
   } ->
   class { '::mongodb::client':
-  } ->
-  exec { 'setup MongoDB admin user':
-    command => $create_admin_user,
-    creates => '/var/lock/mongo_admin_user_lock',
+  }
+
+  if $::cfn_resource_name == 'InstanceA' {
+
+    Anchor['::mongodb::client::end'] ->
+
+    exec { 'setup MongoDB admin user':
+      command => $create_admin_user,
+      creates => '/var/lock/mongo_admin_user_lock',
+    }
   }
 
   contain ::mongodb::server
   contain ::mongodb::client
 
 }
+
