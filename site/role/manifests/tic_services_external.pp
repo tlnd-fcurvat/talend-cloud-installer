246a82bca75f0d39c46dad90650305a6c20873a8
Stefan Baryakov <sbaryakov@talend.com>
2016-11-08 19:55:29 +0100

    nginx for the legacy load balancers setup

--- site/role/manifests/tic_services_external.pp
+++ site/role/manifests/tic_services_external.pp
@@ -5,6 +5,7 @@ class role::tic_services_external {
 
   require ::profile::base
   require ::profile::tic_services
+  require ::profile::tic_services_external::nginx
 
   role::register_role { 'tic_services_external': }
 
