ee794d1f0ddf8feb2b1da55b792d45284094f6fc
Stefan Baryakov <sbaryakov@talend.com>
2016-11-22 16:21:43 +0100

    comment out cloudwatch until we fix duplicate resource breakage

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -15,7 +15,7 @@ class profile::base {
 
   include ::profile::common::concat
   include ::profile::common::accounts
-  include ::profile::common::cloudwatch
+  # include ::profile::common::cloudwatch
 
 
   profile::register_profile { 'base': order => 1, }
