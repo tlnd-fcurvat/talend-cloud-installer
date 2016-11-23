687d55fbde13385ce7533faca0f40affb353ace5
Julien Leloup <jleloup@talend.com>
2016-11-23 11:24:58 +0100

    Temporarily remove puppet-cloudwatch until it is fixed.

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -16,8 +16,8 @@ class profile::base {
   include ::profile::common::concat
   include ::profile::common::accounts
 
-  contain(::profile::common::cloudwatch, ::awscli)
-  Class['::profile::common::cloudwatch'] -> Class['::awscli']
+  #contain(::profile::common::cloudwatch, ::awscli)
+  #Class['::profile::common::cloudwatch'] -> Class['::awscli']
 
   profile::register_profile { 'base': order => 1, }
 
@@ -26,6 +26,7 @@ class profile::base {
   }
 
   if $::ec2_metadata {
+    include ::awscli
     include profile::common::helper_scripts
   }
 
