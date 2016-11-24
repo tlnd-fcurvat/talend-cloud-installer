7243f7caa1616c9a3b4ba3f296e05cdeca39335e
Erwan SEITE <eseite@talend.com>
2016-11-24 14:33:28 +0100

    remove dns management with docker

--- site/profile/manifests/docker_host.pp
+++ site/profile/manifests/docker_host.pp
@@ -15,16 +15,6 @@ class profile::docker_host (
 
   profile::register_profile { 'docker_host': }
 
-  # the dns in vpc is always the 2nd address on the network
-  if is_ipv4_address($::network_eth0) {
-    $t = split($::network_eth0, '\.')
-    $default_resolver = "${t[0]}.${t[1]}.${t[2]}.2"
-  } else {
-    #vagrant mode (test)?
-    warning('docker-dns not resolved, you must be in test mode (kitchen/vagrant)')
-    $default_resolver = undef
-  }
-
   sysctl {
     'net.ipv4.conf.all.route_localnet':
       value => '1';
@@ -34,7 +24,6 @@ class profile::docker_host (
   }
 
   class { '::docker':
-    dns                         => $default_resolver,
     use_upstream_package_source => true,
     require                     => Sysctl['net.ipv4.conf.all.route_localnet', 'vm.max_map_count']
   }
