9a131a18c72864e5f251f7124f0935a5418b8aad
Alexander Grigoriev <agrigoriev@talend.com>
2016-12-02 17:10:19 +0100

    more specs

--- spec/acceptance/dataprep_dataset_spec.rb
+++ spec/acceptance/dataprep_dataset_spec.rb
@@ -4,6 +4,10 @@ describe 'role::dataprep_dataset' do
   it_behaves_like 'profile::base'
   it_behaves_like 'role::defined', 'dataprep_dataset'
 
+  describe service('dataprep-dataset') do
+    it { should be_running }
+  end
+
   describe 'should have java process with correct arguments' do
     subject { command('/usr/bin/pgrep -a java').stdout }
     it { should include '/opt/talend/dataprep' }
