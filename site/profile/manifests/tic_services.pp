7d51bbb82f44af9bd9a62712918e2d754df0c003
Stefan Baryakov <sbaryakov@talend.com>
2017-01-30 22:27:49 +0100

    set iam_internal_elb_dns_name

--- site/profile/manifests/tic_services.pp
+++ site/profile/manifests/tic_services.pp
@@ -191,10 +191,7 @@ class profile::tic_services (
   }
 
   if versioncmp($::ipaas_rt_infra_build_version, '2.0') {
-    class {
-      'tic::services20':
-        iam_service_node       => 'iam-test-node'
-    }
+      include 'tic::services20'
   }
 
 }
