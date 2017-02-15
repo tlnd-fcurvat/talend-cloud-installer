29fd8dccfa49ce373ac08abd61e80beab5dbc304
Alexander Grigoriev <agrigoriev@talend.com>
2017-02-15 12:56:42 +0100

    updating the specs

--- spec/acceptance/tic_services_internal_spec.rb
+++ spec/acceptance/tic_services_internal_spec.rb
@@ -81,4 +81,9 @@ describe 'role::tic_services_internal' do
     it { should be_installed }
   end
 
+  describe 'CMS configuration' do
+    subject { file('/opt/talend/ipaas/rt-infra/etc/org.talend.ipaas.rt.cms.config.cfg').content }
+    it { should include 'karaf/org.ops4j.pax.url.mvn/org.ops4j.pax.url.mvn.repositories=http://{{username}}:{{password}}@10.0.2.12' }
+  end
+
 end
