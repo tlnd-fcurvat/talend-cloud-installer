c88d836197e163edb126a4f9b418e90135768981
Stefan Baryakov <sbaryakov@talend.com>
2017-02-18 14:37:58 +0100

    lint

--- site/role/manifests/management_proxy.pp
+++ site/role/manifests/management_proxy.pp
@@ -11,8 +11,8 @@ class role::management_proxy(
   $elasticsearch_url = "${elasticsearch_url_scheme}://${elasticsearch_host}"
 
   nginx::resource::vhost { 'es-sys':
-    server_name  => ['_'],
-    listen_port  => '8080',
-    proxy        => $elasticsearch_url
+    server_name => ['_'],
+    listen_port => '8080',
+    proxy       => $elasticsearch_url
   }
 }
