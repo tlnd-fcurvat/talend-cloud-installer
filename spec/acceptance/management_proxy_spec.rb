231d5b334394ef60bad1d6f8e7a8c72c899ec382
Alexander Grigoriev <agrigoriev@talend.com>
2017-02-27 12:49:25 +0100

    updating specs

--- spec/acceptance/management_proxy_spec.rb
+++ spec/acceptance/management_proxy_spec.rb
@@ -2,8 +2,19 @@ require 'spec_helper'
 
 describe 'role::management_proxy' do
 
-  describe command('/usr/bin/curl http://localhost:8080/') do
-      its(:stdout) { should include 'You Know, for Search' }
+  describe command('/usr/bin/curl -v http://localhost:8080/') do
+    its(:stdout) { should_not include 'You Know, for Search' }
+    its(:stdout) { should include 'HTTP/1.1 401 Unauthorized' }
+  end
+
+  describe command('/usr/bin/curl -v -u "kibana:mypassword" http://localhost:8080/') do
+    its(:stdout) { should include 'You Know, for Search' }
+    its(:stdout) { should include 'HTTP/1.1 200 OK' }
+  end
+
+  describe file('/etc/nginx/.htpasswd') do
+    it { should be_file }
+    its(:content) { should include 'kibana:' }
   end
 
 end
