f8696a85050144cea5571e488ba44d2f809d4985
Alexander Grigoriev <agrigoriev@talend.com>
2017-03-07 10:20:15 +0100

    specs updated

--- spec/acceptance/shared/docker_host.rb
+++ spec/acceptance/shared/docker_host.rb
@@ -27,6 +27,14 @@ shared_examples 'profile::docker_host' do
   #- Push to your registry
   #  - docker push localhost:5000/test/hello-world:latest
   # Your registry is now ready for this acceptance test
+  describe command('/usr/bin/docker tag hello-world localhost:5000/test/hello-world:latest') do
+    its(:exit_status) { should eq 0 }
+  end
+
+  describe command('/usr/bin/docker push localhost:5000/test/hello-world:latest') do
+    its(:exit_status) { should eq 0 }
+  end
+
   describe command('/usr/bin/docker pull localhost:5000/test/hello-world:latest') do
     its(:exit_status) { should eq 0 }
   end
