748fe2063ad3bf1a1d32ebd6a25d4f6d5da5d6b3
Alexander Grigoriev <agrigoriev@talend.com>
2017-03-07 10:19:57 +0100

    provision docker images/running containers via hiera

--- site/profile/manifests/docker/host.pp
+++ site/profile/manifests/docker/host.pp
@@ -3,11 +3,7 @@
 #
 class profile::docker::host (
 
-  $registry_region,
-  $registry_bucket,
-  $registry_prefix = '',
-  $aws_accesskey   = '',
-  $aws_secretkey   = '',
+  $resolver_ips = [],
 
 ) {
 
@@ -18,46 +14,23 @@ class profile::docker::host (
       value => '1';
 
     'vm.max_map_count':
-      value => '262144' #this value is specific for elasticsearch services needs
+      value => '262144' # this value is specific for elasticsearch services needs
+  }
+
+  if $::network_eth0 {
+    $t                = split($::network_eth0, '\.')
+    $default_resolver = "${t[0]}.${t[1]}.${t[2]}.2"
+  } else {
+    $default_resolver = []
   }
 
   class { '::docker':
+    dns                         => concat($resolver_ips, $default_resolver),
     use_upstream_package_source => true,
-    require                     => Sysctl['net.ipv4.conf.all.route_localnet', 'vm.max_map_count']
+    require                     => Sysctl['net.ipv4.conf.all.route_localnet', 'vm.max_map_count'],
   }
 
-  # Building optionnals container env vars
-  if empty($registry_prefix) {
-    $registry_prefix_env = ''
-  } else {
-    $registry_prefix_env = "REGISTRY_STORAGE_S3_ROOTDIRECTORY=${registry_prefix}"
-  }
-
-  # Only for local acceptance tests with docker used, on AWS, roles and policies for ec2 instances will be sufficient
-  if empty($aws_accesskey) {
-    $aws_accesskey_env = ''
-  } else {
-    $aws_accesskey_env = "REGISTRY_STORAGE_S3_ACCESSKEY=${aws_accesskey}"
-  }
-
-  if empty($aws_secretkey) {
-    $aws_secretkey_env = ''
-  } else {
-    $aws_secretkey_env= "REGISTRY_STORAGE_S3_SECRETKEY=${aws_secretkey}"
-  }
-
-  # Every docker host has a registry listening in localhost:5000
-  docker::run { 'registry':
-    image => 'registry:2',
-    ports => '127.0.0.1:5000:5000',
-    env   => delete( [
-      'REGISTRY_STORAGE=s3',
-      "REGISTRY_STORAGE_S3_REGION=${registry_region}",
-      "REGISTRY_STORAGE_S3_BUCKET=${registry_bucket}",
-      $registry_prefix_env,
-      $aws_accesskey_env,
-      $aws_secretkey_env
-      ], '' )
-  }
+  include ::docker::images
+  include ::docker::run_instance
 
 }
