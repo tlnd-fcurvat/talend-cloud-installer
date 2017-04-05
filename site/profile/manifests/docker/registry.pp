b3d3f16cfe9b3223771b8ddcb57795ed11fd16ae
Alexander Grigoriev <agrigoriev@talend.com>
2017-04-05 14:08:41 +0200

    making option to disable docker registry completely

--- site/profile/manifests/docker/registry.pp
+++ site/profile/manifests/docker/registry.pp
@@ -3,6 +3,7 @@
 #
 class profile::docker::registry (
 
+  $ensure            = 'installed',
   $running           = true,
   $image             = 'registry:2',
   $storage_driver    = 'filesystem',
@@ -17,49 +18,53 @@ class profile::docker::registry (
 
   require ::profile::docker::host
 
-  profile::register_profile { 'docker_registry': }
+  if 'absent' != $ensure {
+    profile::register_profile { 'docker_registry': }
 
-  if 'filesystem' == $storage_driver {
-    validate_absolute_path($filesystem_path)
+    if 'filesystem' == $storage_driver {
+      validate_absolute_path($filesystem_path)
 
-    file { 'Docker registry : ensure filesystem_path':
-      ensure => directory,
-      path   => $filesystem_path,
-    } ->
-    class { '::profile::common::mount_device':
-      device  => $filesystem_device,
-      path    => $filesystem_path,
-      options => 'noatime,nodiratime',
-      before  => Docker::Run['registry'],
+      file { 'Docker registry : ensure filesystem_path':
+        ensure => directory,
+        path   => $filesystem_path,
+      } ->
+      class { '::profile::common::mount_device':
+        device  => $filesystem_device,
+        path    => $filesystem_path,
+        options => 'noatime,nodiratime',
+        before  => Docker::Run['registry'],
+      }
+
+      $options = {
+        'REGISTRY_STORAGE'                          => 'filesystem',
+        'REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY' => '/data',
+      }
+      $volumes = [
+        "${filesystem_path}:/data",
+      ]
+    } elsif 's3' == $storage_driver {
+      validate_string($s3_region)
+      validate_string($s3_bucket)
+      validate_string($s3_prefix)
+
+      $options = {
+        'REGISTRY_STORAGE'                  => 's3',
+        'REGISTRY_STORAGE_S3_REGION'        => $s3_region,
+        'REGISTRY_STORAGE_S3_BUCKET'        => $s3_bucket,
+        'REGISTRY_STORAGE_S3_ROOTDIRECTORY' => $s3_prefix,
+      }
+      $volumes = []
     }
 
-    $options = {
-      'REGISTRY_STORAGE'                          => 'filesystem',
-      'REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY' => '/data',
+    docker::run { 'registry':
+      running => $running,
+      image   => $image,
+      ports   => '127.0.0.1:5000:5000',
+      volumes => $volumes,
+      env     => join_keys_to_values(merge($options, $env), '='),
     }
-    $volumes = [
-      "${filesystem_path}:/data",
-    ]
-  } elsif 's3' == $storage_driver {
-    validate_string($s3_region)
-    validate_string($s3_bucket)
-    validate_string($s3_prefix)
-
-    $options = {
-      'REGISTRY_STORAGE'                  => 's3',
-      'REGISTRY_STORAGE_S3_REGION'        => $s3_region,
-      'REGISTRY_STORAGE_S3_BUCKET'        => $s3_bucket,
-      'REGISTRY_STORAGE_S3_ROOTDIRECTORY' => $s3_prefix,
-    }
-    $volumes = []
-  }
-
-  docker::run { 'registry':
-    running => $running,
-    image   => $image,
-    ports   => '127.0.0.1:5000:5000',
-    volumes => $volumes,
-    env     => join_keys_to_values(merge($options, $env), '='),
+  } else {
+    notice('Skipping Docker Registry initialization due to the ensure parameter was set to absent.')
   }
 
 }
